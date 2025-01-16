25ec02ec5d7cc3189b61b474d3abf5793b52741f
Riccardo Persiani <riccardo@ssvlabs.io>
2025-01-16 16:47:53 +0100

    fix: <- move away from global imports

--- src/BasedAppManager.sol
+++ src/BasedAppManager.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable, Initializable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import "./interfaces/ICore.sol";
-import "./interfaces/IBasedAppManager.sol";
+import {ICore} from "./interfaces/ICore.sol";
+import {IBasedAppManager} from "./interfaces/IBasedAppManager.sol";
 
 /**
  * @title BasedAppManager
