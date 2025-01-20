8dac4b23e26b29eca7fe5b1914390f7436b34827
Riccardo Persiani <riccardo@ssvlabs.io>
2025-01-20 15:40:56 +0100

    feat: <- add GPL-3.0 license

--- src/BasedAppManager.sol
+++ src/BasedAppManager.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.28;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
--- test/BasedAppManager.t.sol
+++ test/BasedAppManager.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.28;
 
 import {Test, console} from "forge-std/Test.sol";
