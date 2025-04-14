afeda25f3f78c231ce2811e04d866941a77abbde
Riccardo Persiani <riccardo@ssvlabs.io>
2025-04-14 09:58:15 +0200

    chore: <- fmt

--- src/core/interfaces/ISSVBasedApps.sol
+++ src/core/interfaces/ISSVBasedApps.sol
@@ -3,8 +3,6 @@ pragma solidity 0.8.29;
 
 import {IStrategyManager} from "@ssv/src/core/interfaces/IStrategyManager.sol";
 import {IBasedAppManager} from "@ssv/src/core/interfaces/IBasedAppManager.sol";
-import {ISlashingManager} from "@ssv/src/core/interfaces/ISlashingManager.sol";
-import {IDelegationManager} from "@ssv/src/core/interfaces/IDelegationManager.sol";
 import {IProtocolManager} from "@ssv/src/core/interfaces/IProtocolManager.sol";
 import {SSVCoreModules} from "@ssv/src/core/libraries/CoreStorageLib.sol";
 import {StorageProtocol} from "@ssv/src/core/libraries/SSVBasedAppsStorageProtocol.sol";
