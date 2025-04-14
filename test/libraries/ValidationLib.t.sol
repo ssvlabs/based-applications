2af170d751a4aaab54f43d38670ff917a9e2fadc
Riccardo Persiani <riccardo@ssvlabs.io>
2025-04-14 15:39:59 +0200

    chore: <- update package.json scripts

--- test/libraries/ValidationLib.t.sol
+++ test/libraries/ValidationLib.t.sol
@@ -5,12 +5,12 @@ import {ValidationLib} from "@ssv/src/core/libraries/ValidationLib.sol";
 import {Setup} from "@ssv/test/helpers/Setup.t.sol";
 
 contract ValidationLibTest is Setup {
-    function testValidatePercentage() public {
+    function testValidatePercentage() public pure {
         ValidationLib.validatePercentage(0);
         ValidationLib.validatePercentage(1e4);
     }
 
-    function testValidatePercentageWithZero() public {
+    function testValidatePercentageWithZero() public pure {
         ValidationLib.validatePercentageAndNonZero(1e4);
     }
 }
