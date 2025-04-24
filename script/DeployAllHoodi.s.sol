f64ffb4563c8dea846e67cf40896a8d31c8fa140
Marco Tabasco <matabasco@gmail.com>
2025-04-24 12:37:56 +0200

    chore: rename testnet -> prod

--- script/DeployAllHoodi.s.sol
+++ script/DeployAllHoodi.s.sol
@@ -5,16 +5,16 @@ import { Script } from "forge-std/Script.sol";
 import { DeployAll } from "./DeployAll.sol";
 
 contract DeployAllHoodi is Script, DeployAll {
-    function run(bool isTestnet) external {
+    function run(bool isProd) external {
         if (block.chainid != 560_048) {
-            revert("This script is only for the Hoodi testnet");
+            revert("This script is only for the Hoodi prod");
         }
 
         string memory cfgPath;
         string memory outPath;
-        if (isTestnet) {
-            cfgPath = "script/config/hoodi_testnet.json";
-            outPath = "artifacts/deploy-hoodi-testnet.json";
+        if (isProd) {
+            cfgPath = "script/config/hoodi_prod.json";
+            outPath = "artifacts/deploy-hoodi-prod.json";
         } else {
             cfgPath = "script/config/hoodi_stage.json";
             outPath = "artifacts/deploy-hoodi-stage.json";
