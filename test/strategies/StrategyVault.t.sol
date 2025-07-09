d1c3e81877b53e29e87afc8ce4b70983e423f930
Riccardo Persiani <riccardo@ssvlabs.io>
2025-07-09 10:31:06 +0200

    test: 100% coverage

--- test/strategies/StrategyVault.t.sol
+++ test/strategies/StrategyVault.t.sol
@@ -30,5 +30,50 @@ contract StrategyVaultTest is StrategyManagerTest {
         IStrategyVault(strategyAddress).withdrawETH(1, address(ATTACKER));
     }
 
+    function testRevertFailTryingWithdrawalETH() public {
+        testCreateStrategyAndSingleDeposit(1 ether);
+        vm.startPrank(address(proxiedManager));
+        (address strategyAddress, , ) = proxiedManager.strategies(STRATEGY1);
+        vm.expectRevert(
+            abi.encodeWithSelector(IStrategyVault.ETHTransferFailed.selector)
+        );
+        // this results in EvmError: OutOfFunds
+        IStrategyVault(strategyAddress).withdrawETH(1, address(proxiedManager));
+    }
+
+    function testRevertZeroAmountWithdrawal() public {
+        testCreateStrategyAndSingleDeposit(1 ether);
+        vm.startPrank(address(proxiedManager));
+        (address strategyAddress, , ) = proxiedManager.strategies(STRATEGY1);
+        vm.expectRevert(
+            abi.encodeWithSelector(IStrategyVault.InvalidZeroAmount.selector)
+        );
+        IStrategyVault(strategyAddress).withdraw(
+            erc20mock,
+            0,
+            address(proxiedManager)
+        );
+    }
+
+    function testRevertZeroAmountWithdrawalETH() public {
+        testCreateStrategyAndSingleDeposit(1 ether);
+        vm.startPrank(address(proxiedManager));
+        (address strategyAddress, , ) = proxiedManager.strategies(STRATEGY1);
+        vm.expectRevert(
+            abi.encodeWithSelector(IStrategyVault.InvalidZeroAmount.selector)
+        );
+        IStrategyVault(strategyAddress).withdrawETH(0, address(proxiedManager));
+    }
+
+    function testRevertSendEth() public {
+        testCreateStrategyAndSingleDeposit(1 ether);
+        vm.startPrank(ATTACKER);
+        (address strategyAddress, , ) = proxiedManager.strategies(STRATEGY1);
+        vm.expectRevert(
+            abi.encodeWithSelector(IStrategyVault.UnauthorizedCaller.selector)
+        );
+        payable(strategyAddress).transfer(1 ether);
+    }
+
     // todo: test if they can receive eth from other users
 }
